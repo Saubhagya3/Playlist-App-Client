2a3973164fada7c06c5e989d680d7c2a92f3edb5
Saubhagya3 <saubhagyathapa3@gmail.com>
2022-03-09 16:50:14 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><a href="/">Videos</a></li>
+            <li><a href="/stuff">Playlists</a></li>
+          </ul>
+          <div className="content">
+             
+          </div>
+        </div>
+  );
+}
+
+export default App;
